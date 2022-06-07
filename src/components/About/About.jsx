0fb236de82f43118ff34b0276aa490b8bd9b9644
Panescu Cosmin <1pane.cosmin@gmail.com>
2022-06-07 21:19:02 +0300

    small changes

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
                     </div>
                     <div className="about-details">
                         <div className="about-info">
-                            <p>Hello! My name is Pănescu Cosmin and I like to build things for the web. The first contact with programming took place in 2019, when I learned the basics of programming, but my interest in web development started back in 2020, during the quarantine. Since then, I have been learning the basics of web programming on my own, buying and following, then, advanced courses to improve my web development skills. Starting with being self-taught, I realized that this study model suits me best and helps me move forward much faster, because I can focus on exactly  what I need. Even so, I am a first-year computer science student at the University of Bucharest, and in the next academic year I will enroll in the specialization of Cybernetics at the University of Bucharest.
+                            <p>Hello! My name is Pănescu Cosmin and I like to build things for the web. I want to start by saying that I am a self-driven and self-motivated person, because I believe that these characteristics have helped me enormously to get to the point where I am, both professionally and personally. Starting with being self-taught, I realized that this study model suits me best and helps me move forward much faster, because I can focus on exactly  what I need. Even so, I am a first-year Computer Science student at the University of Bucharest, and in the next academic year I will enroll in the specialization of Cybernetics, at the same University.
                             </p>
                             <p>The last and most important course I took was <span>"The Frontend Developer Career Path"</span>, from <a className='scrimba-btn' href="https://scrimba.com/learn/frontend" target="_blank">Scrimba &#8592;</a></p>
                             <hr style={{ width: "100%" }} />
@@ -31,6 +31,7 @@ const About = () => {
                                     <li>JavaScript (ES6)</li>
                                     <li>HTML</li>
                                     <li>CSS</li>
+                                    <li>Python (beginner)</li>
                                 </ul>
                                 <ul>
                                     <li><span>Libraries & Frameworks:</span></li>
