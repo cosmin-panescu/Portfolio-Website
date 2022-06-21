a83cd0cb1db57fc53d31cc667cb56c97cae16d48
Panescu Cosmin <1pane.cosmin@gmail.com>
2022-06-22 00:22:32 +0300

    updating project section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,53 +1,13 @@
 import React from 'react'
 import "./Projects.scss"
 import SectionHeader from "../SectionHeader/SectionHeader"
-import netflixCloneImg from "../../assets/netflix-clone.png"
-import modernToDoImg from "../../assets/modern-to-do.png"
-import browserDashboardImg from '../../assets/browser-dashboard.png'
-import meditationAppImg from "../../assets/meditation-app.png"
-import cryptoTrackerImg from "../../assets/crypto-tracker.png"
 import { Element } from 'react-scroll'
-
-// prpojects info
-const projectsInfo = [
-    {
-        src: netflixCloneImg,
-        title: "Netflix Clone",
-        description: "A clone of Netflix, using the API. Thus, we can display movies and series by category, including the 'Most Viewed' and 'Netflix Originals' categories. At the same time, each category is updated weekly, displaying updated content",
-        live: "https://cosmin-panescu.github.io/Netflix-Clone/",
-        code: "https://github.com/cosmin-panescu/Netflix-Clone"
-    },
-    {
-        src: cryptoTrackerImg,
-        title: "Crypto Tracker",
-        description: 'A multi-page application that displays cryptocurrencies. The first page displays the cryptocurrencies, and the second page displays detailed information about the cryptocurrency chosen by the user. You can also search for any currency in real time.',
-        live: "https://cosmin-panescu.github.io/Crypto-Tracker/",
-        code: "https://github.com/cosmin-panescu/Crypto-Tracker"
-    },
-    {
-        src: modernToDoImg,
-        title: "Modern To Do",
-        description: "This project includes features like normal to-do-list options, dark/light theme, optional background music, background 'rain' effect, display the current date",
-        live: "https://cosmin-panescu.github.io/Modern-To-Do-List/",
-        code: "https://github.com/cosmin-panescu/Modern-To-Do-List"
-    },
-    {
-        src: browserDashboardImg,
-        title: "API Browser Dashboard",
-        description: "Chrome NewTab Dashboard, using HTML, CSS and JavaScript. Almost all of the JavaScript part is represented by the API (such as: random background, weather/location, search and display of cryptocurrencies)",
-        live: "https://cosmin-panescu.github.io/Chrome-Dashboard/",
-        code: "https://github.com/cosmin-panescu/Chrome-Dashboard"
-    },
-    {
-        src: meditationAppImg,
-        title: "Meditation App",
-        description: "The application allows the selection of the desired period for meditation, as well as the two available options: the sound of the sea and the sound of rain, changing at the same time the video in the background.",
-        live: "https://cosmin-panescu.github.io/Meditation-App/",
-        code: "https://github.com/cosmin-panescu/Meditation-App"
-    },
-]
+import { projectsInfo, moreProjectsInfo } from '../../utils/allProjects'
+import { useState } from 'react'
 
 const Projects = () => {
+    const [more, setMore] = useState(false);
+
     return (
         <Element name='portfolio'>
             <div className='projects' data-scroll-section>
@@ -68,10 +28,28 @@ const Projects = () => {
                                 <img src={project.src} />
                             </div>
                         )
-                    })
-                    }
+                    })}
+
+                    {moreProjectsInfo.map((project) => {
+                        return (
+                            <div key={project.title} className={`${more ? 'project-item' : 'project-item more'}`}>
+                                <div className="project-details">
+                                    <h1>{project.title}</h1>
+                                    <p>{project.description}</p>
+                                    <div className="project-link">
+                                        <a className='btn-dark' target="_blank" href={project.live}>Live</a>
+                                        <a className='btn-dark' target="_blank" href={project.code}>Code</a>
+                                    </div>
+                                </div>
+                                <img src={project.src} />
+                            </div>
+                        )
+                    })}
+
                     {/* GitHub link for more projects */}
-                    <a target="_blank" href="https://github.com/cosmin-panescu" className="more-projects-btn btn-dark">More Projects</a>
+                    <a role="link" className="more-projects-btn btn-dark" onClick={() => setMore(!more)}>
+                        {more ? "Show Less" : "Show More"}
+                    </a>
                 </div>
             </div>
         </Element>
---
+++ src/utils/allProjects.js
@@ -0,0 +1,54 @@
+import netflixCloneImg from "../assets/netflix-clone.png"
+import modernToDoImg from "../assets/modern-to-do.png"
+import browserDashboardImg from '../assets/browser-dashboard.png'
+import meditationAppImg from "../assets/meditation-app.png"
+import cryptoTrackerImg from "../assets/crypto-tracker.png"
+import webToolsImg from "../assets/web-tools.png"
+
+export const projectsInfo = [
+    {
+        src: netflixCloneImg,
+        title: "Netflix Clone",
+        description: "A clone of Netflix, using the API. Thus, we can display movies and series by category, including the 'Most Viewed' and 'Netflix Originals' categories. At the same time, each category is updated weekly, displaying updated content",
+        live: "https://cosmin-panescu.github.io/Netflix-Clone/",
+        code: "https://github.com/cosmin-panescu/Netflix-Clone"
+    },
+    {
+        src: cryptoTrackerImg,
+        title: "Crypto Tracker",
+        description: 'A multi-page application that displays cryptocurrencies. The first page displays the cryptocurrencies, and the second page displays detailed information about the cryptocurrency chosen by the user. You can also search for any currency in real time',
+        live: "https://cosmin-panescu.github.io/Crypto-Tracker/",
+        code: "https://github.com/cosmin-panescu/Crypto-Tracker"
+    },
+    {
+        src: webToolsImg,
+        title: "Web Tools",
+        description: "This application contains three tools: a complex password generator with multiple possibilities, a generator of real QR codes for links and a generator for real shortening of links",
+        live: "https://cosmin-panescu.github.io/Web-Tools/",
+        code: "https://github.com/cosmin-panescu/Web-Tools"
+    },
+    {
+        src: browserDashboardImg,
+        title: "API Browser Dashboard",
+        description: "Chrome NewTab Dashboard, using HTML, CSS and JavaScript. Almost all of the JavaScript part is represented by the API (such as: random background, weather/location, search and display of cryptocurrencies)",
+        live: "https://cosmin-panescu.github.io/Chrome-Dashboard/",
+        code: "https://github.com/cosmin-panescu/Chrome-Dashboard"
+    },
+]
+
+export const moreProjectsInfo = [
+    {
+        src: meditationAppImg,
+        title: "Meditation App",
+        description: "The application allows the selection of the desired period for meditation, as well as the two available options: the sound of the sea and the sound of rain, changing at the same time the video in the background",
+        live: "https://cosmin-panescu.github.io/Meditation-App/",
+        code: "https://github.com/cosmin-panescu/Meditation-App"
+    },
+    {
+        src: modernToDoImg,
+        title: "Modern To Do",
+        description: "This project includes features like normal to-do-list options, dark/light theme, optional background music, background 'rain' effect, display the current date",
+        live: "https://cosmin-panescu.github.io/Modern-To-Do-List/",
+        code: "https://github.com/cosmin-panescu/Modern-To-Do-List"
+    },
+]
