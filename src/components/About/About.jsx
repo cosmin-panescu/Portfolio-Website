bbaf14a1a8342d539a2d6477ac86cb5efc2cc8f2
Panescu Cosmin <1pane.cosmin@gmail.com>
2022-06-27 18:04:10 +0300

    CV and Skills modifications

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -21,32 +21,32 @@ const About = () => {
                     </div>
                     <div className="about-details">
                         <div className="about-info">
-                            <p>Hello! My name is Pănescu Cosmin and I like to build things for the web. I want to start by saying that I am a self-driven and self-motivated person, because I believe that these characteristics have helped me enormously to get to the point where I am, both professionally and personally. Starting with being self-taught, I realized that this study model suits me best and helps me move forward much faster, because I can focus on exactly  what I need. Even so, I am a first-year Computer Science student at the University of Bucharest, and in the next academic year I will enroll in the specialization of Cybernetics, at the same University.
+                            <p>Hello!My name is Pănescu Cosmin and I like to build things for the web and lately I've been learning about Web3 (blockchain, smart-contracts, DeFi, dApp), because it represents the next generation of the internet. I want to start by saying that I am a self-driven and self-motivated person, because I believe that these characteristics have helped me enormously to get to the point where I am, both professionally and personally. Starting with being self-taught, I realized that this study model suits me best and helps me move forward much faster, because I can focus on exactly what I need. Even so, I am a first-year Computer Science student at the University of Bucharest, and in the next academic year I will enroll in the specialization of Cybernetics, at the same University.
                             </p>
-                            <p>The last and most important course I took was <span>"The Frontend Developer Career Path"</span>, from <a className='scrimba-btn' href="https://scrimba.com/learn/frontend" target="_blank">Scrimba &#8592;</a></p>
                             <hr style={{ width: "100%" }} />
                             <div className="skills">
                                 <ul>
                                     <li><span>Programming Languages:</span></li>
-                                    <li>JavaScript (ES6)</li>
                                     <li>HTML</li>
                                     <li>CSS</li>
+                                    <li>JavaScript</li>
+                                    <li>Solidity</li>
                                     <li>Python (beginner)</li>
                                 </ul>
                                 <ul>
                                     <li><span>Libraries & Frameworks:</span></li>
                                     <li>React</li>
+                                    <li>Next.js</li>
                                     <li>Bootstrap</li>
-                                    <li>Sass</li>
+                                    <li>SCSS</li>
                                 </ul>
                                 <ul>
-                                    <li><span>Tools and Platforms:</span></li>
+                                    <li><span>Tools & Platforms:</span></li>
                                     <li>Git</li>
                                     <li>GitHub</li>
+                                    <li>Web3.js</li>
                                     <li>Figma</li>
                                     <li>Webpack</li>
-                                    <li>VSC</li>
-                                    <li>Canva</li>
                                 </ul>
                             </div>
                             {/* CV download */}
