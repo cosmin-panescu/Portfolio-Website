59e4283d685b8c41074d1a864ac29faadadce096
Panescu Cosmin <1pane.cosmin@gmail.com>
2022-07-02 19:32:46 +0300

    update

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -21,7 +21,7 @@ const About = () => {
                     </div>
                     <div className="about-details">
                         <div className="about-info">
-                            <p>Hello!My name is Pănescu Cosmin and I like to build things for the web and lately I've been learning about Web3 (blockchain, smart-contracts, DeFi, dApp), because it represents the next generation of the internet. I want to start by saying that I am a self-driven and self-motivated person, because I believe that these characteristics have helped me enormously to get to the point where I am, both professionally and personally. Starting with being self-taught, I realized that this study model suits me best and helps me move forward much faster, because I can focus on exactly what I need. Even so, I am a first-year Computer Science student at the University of Bucharest, and in the next academic year I will enroll in the specialization of Cybernetics, at the same University.
+                            <p>Hello! My name is Pănescu Cosmin and I like to build things for the web and lately I've been learning about Web3 (blockchain, smart-contracts, DeFi, dApp), because it represents the next generation of the internet. I want to start by saying that I am a self-driven and self-motivated person, because I believe that these characteristics have helped me enormously to get to the point where I am, both professionally and personally. Starting with being self-taught, I realized that this study model suits me best and helps me move forward much faster, because I can focus on exactly what I need. Even so, I am a first-year Computer Science student at the University of Bucharest, and in the next academic year I will enroll in the specialization of Cybernetics, at the same University.
                             </p>
                             <hr style={{ width: "100%" }} />
                             <div className="skills">
@@ -30,21 +30,22 @@ const About = () => {
                                     <li>HTML</li>
                                     <li>CSS</li>
                                     <li>JavaScript</li>
-                                    <li>Solidity</li>
+                                    <li>Solidity (beginner)</li>
                                     <li>Python (beginner)</li>
                                 </ul>
                                 <ul>
                                     <li><span>Libraries & Frameworks:</span></li>
                                     <li>React</li>
+                                    <li>Node.js</li>
+                                    <li>Express.js</li>
                                     <li>Next.js</li>
+                                    <li>SASS</li>
                                     <li>Bootstrap</li>
-                                    <li>SCSS</li>
                                 </ul>
                                 <ul>
                                     <li><span>Tools & Platforms:</span></li>
                                     <li>Git</li>
                                     <li>GitHub</li>
-                                    <li>Web3.js</li>
                                     <li>Figma</li>
                                     <li>Webpack</li>
                                 </ul>
