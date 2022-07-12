f9c7977f4f227113f7c21222c914519033fc376c
Panescu Cosmin <1pane.cosmin@gmail.com>
2022-07-12 14:50:45 +0300

    .

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -21,24 +21,20 @@ const About = () => {
                     </div>
                     <div className="about-details">
                         <div className="about-info">
-                            <p>Hello! My name is Pănescu Cosmin and I like to build things for the web and lately I've been learning about Web3 (blockchain, smart-contracts, DeFi, dApp), because it represents the next generation of the internet. I want to start by saying that I am a self-driven and self-motivated person, because I believe that these characteristics have helped me enormously to get to the point where I am, both professionally and personally. Starting with being self-taught, I realized that this study model suits me best and helps me move forward much faster, because I can focus on exactly what I need. Even so, I studied Computer Science for a year at the University of Bucharest, and now I transferred to the specialization of Cybernetics, from the Faculty of Administration and Business, at the same University
+                            <p>Hello! My name is Pănescu Cosmin and I like to build things for the web and lately I've been learning about Web3 (blockchain, smart-contracts, DeFi, dApp), because it represents the next generation of the internet. I want to start by saying that I am a self-driven and self-motivated person, because I believe that these characteristics have helped me enormously to get to the point where I am, both professionally and personally. Starting with being self-taught, I realized that this study model suits me best and helps me move forward much faster, because I can focus on exactly what I need. Even so, I studied Computer Science for a year at the University of Bucharest, and now I transferred to the specialization of Cybernetics, from the Faculty of Administration and Business, at the same University.
                             </p>
                             <hr style={{ width: "100%" }} />
+                            <h4>Main skills:</h4>
                             <div className="skills">
                                 <ul>
                                     <li><span>Programming Languages:</span></li>
                                     <li>HTML</li>
                                     <li>CSS</li>
                                     <li>JavaScript</li>
-                                    <li>SQL</li>
-                                    <li>Solidity (beginner)</li>
-                                    <li>Python (beginner)</li>
                                 </ul>
                                 <ul>
                                     <li><span>Libraries & Frameworks:</span></li>
                                     <li>React</li>
-                                    <li>Node.js</li>
-                                    <li>Express.js</li>
                                     <li>Next.js</li>
                                     <li>SASS</li>
                                     <li>Bootstrap</li>
@@ -48,7 +44,28 @@ const About = () => {
                                     <li>Git</li>
                                     <li>GitHub</li>
                                     <li>Figma</li>
+                                    <li>Axios</li>
                                     <li>Webpack</li>
+                                </ul>
+                            </div>
+                            <h4>Knowledge about:</h4>
+                            <div className="skills">
+                                <ul>
+                                    <li><span>Programming Languages:</span></li>
+                                    <li>SQL</li>
+                                    <li>Python</li>
+                                    <li>Solidity</li>
+                                </ul>
+                                <ul>
+                                    <li><span>Libraries & Frameworks:</span></li>
+                                    <li>Node.js</li>
+                                    <li>Express.js</li>
+                                    <li>MongoDB</li>
+                                </ul>
+                                <ul>
+                                    <li><span>Tools & Platforms:</span></li>
+                                    <li>Postman</li>
+                                    <li>Photoshop</li>
                                 </ul>
                             </div>
                             {/* CV download */}
