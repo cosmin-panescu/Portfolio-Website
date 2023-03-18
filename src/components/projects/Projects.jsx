a7a10734d902c6516c7ec9c9803a4656fd4fb841
Panescu Cosmin <1panescu.cosmin@gmail.com>
2023-03-18 18:39:05 +0200

    Add -load more projects- button

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,11 +1,13 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import './Projects.scss';
-import img from '../../assets/about-image.jpg';
-import { projectsInfo } from './projectsInfo';
+import { moreProjectsInfo, projectsInfo } from './projectsInfo';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 const Projects = () => {
+    // load more projects state
+    const [more, setMore] = useState(false);
+
     useEffect(() => {
         AOS.init({
             duration: 1500
@@ -44,6 +46,44 @@ const Projects = () => {
                         )
                     })}
                 </div>
+
+{/* Load more projects */}
+                {more && (
+                    <div className="project-content">
+                        {/* display each project */}
+                        {moreProjectsInfo.map((project, index) => {
+                            return (
+                                <div data-aos="fade-up" className="project-box" key={index}>
+                                    <div className="project-image">
+                                        <img src={project.src} alt={project.alt} />
+                                    </div>
+                                    <h3>{project.title}</h3>
+                                    <div className="project-buttons">
+                                        <a href={project.live} target="_blank" className="project-button">
+                                            Live
+                                            <i className="uil uil-arrow-right project-button-icon"></i>
+                                        </a>
+                                        {
+                                            project.codeLink &&
+                                            <a href={project.code} target="_blank" className="project-button">
+                                                Code
+                                                <i className="uil uil-arrow-right project-button-icon"></i>
+                                            </a>
+                                        }
+                                    </div>
+                                </div>
+                            )
+                        })}
+                    </div>
+                )}
+
+                <button
+                    onClick={() => setMore(!more)}
+                    className="button button-more"
+                >
+                    {more ? "See Less" : "Load More"}
+                    <i className="uil uil-arrow-right project-button-icon"></i>
+                </button>
 
                 <h3 className='projects-more'>More projects on <a href="https://github.com/cosmin-panescu?tab=repositories" target="_blank">GitHub</a></h3>
             </div>
--- src/components/projects/projectsInfo.jsx
+++ src/components/projects/projectsInfo.jsx
@@ -5,6 +5,7 @@ import webToolsImg from "../../assets/web-tools.png"
 import digitalMarketingAgencyImg from '../../assets/digital-marketing-agency.png'
 import notesAppImg from '../../assets/notes-app.png'
 import chromeExtensionImg from '../../assets/chrome-extension.png'
+import romanianFestivalsImg from '../../assets/romanian-festivals.png'
 
 export const projectsInfo = [
     {
@@ -22,6 +23,14 @@ export const projectsInfo = [
         alt: "Chrome New Tab Extension",
         live: "https://cosmin-panescu.github.io/Chrome-Extension-NewTab/",
         code: "https://github.com/cosmin-panescu/Chrome-Extension-NewTab"
+    },
+    {
+        src: romanianFestivalsImg,
+        alt: "Romanian Festivals Website",
+        title: "Romanian Festivals",
+        codeLink: true,
+        live: "https://romania-events-website.vercel.app/",
+        code: "https://github.com/cosmin-panescu/Romania-Events-Website"
     },
     {
         src: notesAppImg,
@@ -47,6 +56,9 @@ export const projectsInfo = [
         live: "https://cosmin-panescu.github.io/Web-Tools/",
         code: "https://github.com/cosmin-panescu/Web-Tools"
     },
+]
+
+export const moreProjectsInfo = [
     {
         src: modernToDoImg,
         alt: "Modern To Do App Project",
